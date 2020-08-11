5f0688da282e6aeef605c574ec83a2e1eb086007
jackchuka <jack16.m.u@gmail.com>
2020-08-11 19:44:12 +0900

    fix: prezto symlink

--- prezto.sh
+++ prezto.sh
@@ -5,8 +5,9 @@ if [ ! -d ${ZDOTDIR:-$HOME}/.zprezto ]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
     # symlink files
-    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-        ln -s "$rcfile" "${ZDOTDIR:$-HOME}/.${rcfile:t}"
+    setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}/.zprezto/runcoms/^README.md(.N)"; do
+        ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
     done
 
     # update zpreztorc
