20fe15bef3bfdcaea5fbb0cfe5c7372426e4250e
jackchuka <jack16.m.u@gmail.com>
2023-02-24 14:49:16 +0900

    add: anyenv plugin update

--- anyenv.sh
+++ anyenv.sh
@@ -2,3 +2,5 @@
 echo "Installing anyenv..."
 
 git clone https://github.com/riywo/anyenv ~/.anyenv
+mkdir -p $(anyenv root)/plugins
+git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
